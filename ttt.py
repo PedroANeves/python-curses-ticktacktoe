3f20bb87b83007055bec417c9517c7aba436e25c
Pedro A. Neves <neves.pedro.augusto@gmail.com>
2018-02-16 05:47:10 -0200

    initial commit

---
+++ ttt.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+import sys,os
+import curses
+
+def ttt(stdscr):
+
+    key = 0
+
+    MAX_SIZE_X = 3
+    MAX_SIZE_Y = 3
+
+    board_pos_x = 0
+    board_pos_y = 0
+
+    screen_pos_x = 0
+    screen_pos_y = 0
+
+    board_row =" | | "
+    board_division ="-+-+-"
+
+    stdscr.clear()
+    stdscr.refresh()
+
+    #press 'q' to exit
+    while (key != ord('q')):
+
+
+        stdscr.clear()
+        height, width = stdscr.getmaxyx()
+
+        #manages input
+        if key == curses.KEY_RIGHT:
+            board_pos_x = board_pos_x + 1
+        elif key == curses.KEY_LEFT:
+            board_pos_x = board_pos_x - 1
+        elif key == curses.KEY_DOWN:
+            board_pos_y = board_pos_y + 1
+        elif key == curses.KEY_UP:
+            board_pos_y = board_pos_y - 1
+
+
+        #clamps board_pos_x and board_pos_y
+        board_pos_x = max(board_pos_x,0)
+        board_pos_x = min(board_pos_x,MAX_SIZE_X-1)
+        board_pos_y = max(board_pos_y,0)
+        board_pos_y = min(board_pos_y,MAX_SIZE_Y-1)
+
+        #build screen
+        info_bar = "X:{} Y:{} | key: {}".format(board_pos_x, board_pos_y, key)
+
+        #draw screen
+        stdscr.addstr(6,1,info_bar)
+
+        stdscr.addstr(0,0,board_row)
+        stdscr.addstr(1,0,board_division)
+        stdscr.addstr(2,0,board_row)
+        stdscr.addstr(3,0,board_division)
+        stdscr.addstr(4,0,board_row)
+
+        #moves cursor
+        screen_pos_x = board_pos_x * 2
+        screen_pos_y = board_pos_y * 2
+        stdscr.move(screen_pos_y, screen_pos_x)
+
+        #refresh screen
+        stdscr.refresh()
+        #gets input
+        key = stdscr.getch()
+
+def main():
+    curses.wrapper(ttt)
+
+if __name__ == "__main__":
+    main()
